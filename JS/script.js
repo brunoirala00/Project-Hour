de322b0b981101bceb4edc20018cd35f0226a4a9
Bruno Irala <bruno.irala00@gmail.com>
2021-08-19 07:57:07 -0300

    Update spacing between lines

--- JS/script.js
+++ JS/script.js
@@ -5,15 +5,18 @@ function loading() {
     var data = new Date()
     var hour = data.getHours()
     var minute = data.getMinutes()
-    msg.innerHTML = `Now is ${hour}h${minute}`
+    msg.innerHTML = `Now is ${hour}h${minute}.`
+    
     if (hour >= 0 && hour < 12) {
         msgWelcome.innerHTML = '<strong> Good morning! </strong>'
         img.src = 'morning.jpg'
         document.body.style.background = '#55C0EF'
+        
     } else if (hour >= 12 && hour < 18) {
         msgWelcome.innerHTML = '<strong> Good afternoon! </strong>'
         img.src = 'afternoon.jpg'
         document.body.style.background = '#f69341'
+        
     } else {
         msgWelcome.innerHTML = '<strong> Good night! </strong>'
         img.src = 'night.jpg'
